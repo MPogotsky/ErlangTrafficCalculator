d263fdcc409cf1c06191d79bcd6ed2a816e475eb
Matsvei Pahotski <matsvei.pahotski@gmail.com>
2021-11-27 23:29:13 +0100

    fix main

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-def erlang_model_B(A, N):
-    if N == 0:
-        return 1
-    else:
-        return (A * erlang_model_B(A, N - 1)) / (A * erlang_model_B(A, N - 1) + N)
-
+import UI
 
 if __name__ == '__main__':
-    average_traffic = int(input('Please, enter the average traffic value in erlangs: '))
-    lines = int(input('Please, enter the number of lines: '))
-    probability_of_blocking = round(erlang_model_B(average_traffic, lines), 3)
-    print(f'The probability of blocking is : {probability_of_blocking}')
-
-
+    UI.MainProgram()
