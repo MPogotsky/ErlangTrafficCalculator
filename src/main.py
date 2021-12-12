2bc7d6d396a766457f5bb0680ec4ddaf9d8389a8
Matsvei Pahotski <matsvei.pahotski@gmail.com>
2021-12-12 23:18:29 +0100

    fix main menu buttons

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from src import UI
 from Calculator import Calculator
 
 if __name__ == '__main__':
@@ -7,9 +6,13 @@ if __name__ == '__main__':
     window.title('Erlang calculator')
     window.geometry("400x300+10+10")
 
-    main_window_label = Label(window, text='Main window')
-    main_window_label.place(x=100, y=50)
+    main_window_label = Label(window, text='Main menu', font="Helvetica 16 bold")
+    main_window_label.pack(pady=50)
+
     calculator_button = Button(window, text='Calculator', command=lambda: Calculator(window))
-    calculator_button.place(x=180, y=150)
+    calculator_button.pack(pady=5)
+
+    information_button = Button(window, text='Information', command=lambda: print("Information"))
+    information_button.pack(pady=5)
 
     window.mainloop()
