5cf4686ec188a6ab6b1b8aa36f82c6a90f380946
Matsvei Pahotski <matsvei.pahotski@gmail.com>
2021-12-12 23:33:40 +0100

    Fix buttons for calculator menu

--- src/Calculator.py
+++ src/Calculator.py
@@ -19,19 +19,19 @@ class Calculator:
 
     def calculatorWindow(self):
         average_traffic_label = Label(self.window, text='Average traffic')
-        number_of_lines_label = Label(self.window, text='Number of lines')
-        prob_of_blocking_label = Label(self.window, text='Probability of blocking')
+        average_traffic_label.pack(pady=20)
+        self.__average_traffic_text_field.pack()
 
-        average_traffic_label.place(x=100, y=50)
-        self.__average_traffic_text_field.place(x=200, y=50)
-        number_of_lines_label.place(x=100, y=100)
-        self.__number_of_lines_text_field.place(x=200, y=100)
+        number_of_lines_label = Label(self.window, text='Number of lines')
+        number_of_lines_label.pack(pady=20)
+        self.__number_of_lines_text_field.pack()
 
         calculate_button = Button(self.window, text='Calculate', command=self.calculateTheResult)
-        calculate_button.place(x=180, y=150)
+        calculate_button.pack(pady=20)
 
-        prob_of_blocking_label.place(x=70, y=200)
-        self.__result_text_field.place(x=200, y=200)
+        prob_of_blocking_label = Label(self.window, text='Probability of blocking')
+        prob_of_blocking_label.pack()
+        self.__result_text_field.pack()
 
     def calculateTheResult(self):
         self.__result_text_field.delete(0, 'end')
