56ac0fae2fe20d063012114749192ccb19e06d73
Matsvei Pahotski <matsvei.pahotski@gmail.com>
2022-01-16 15:23:33 +0100

    fix UI, change cutton size, add creator

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 from src.Calculator import Calculator
 from src.Information import Information
@@ -7,11 +8,16 @@ def controller(window):
     main_window_label = Label(window, text='Erlang Traffic Calculator \n\n', font="Helvetica 16 bold")
     main_window_label.pack(pady=50)
 
-    calculator_button = Button(window, text='Calculator', command=lambda: Calculator(controller, window))
+    calculator_button = Button(window, text='Calculator', height=3, width=20,
+                               command=lambda: Calculator(controller, window))
     calculator_button.pack(pady=5)
 
-    information_button = Button(window, text='Information', command=lambda: Information(controller, window))
+    information_button = Button(window, text='Information', height=3, width=20,
+                                command=lambda: Information(controller, window))
     information_button.pack(pady=5)
+
+    creator_text = Label(window, text='by Matsvei Pahotski, 2022', font="Helvetica 9")
+    creator_text.pack(side=BOTTOM)
 
 
 if __name__ == '__main__':
@@ -21,4 +27,3 @@ if __name__ == '__main__':
     main_window.geometry("500x450+10+10")
     controller(main_window)
     main_window.mainloop()
-
