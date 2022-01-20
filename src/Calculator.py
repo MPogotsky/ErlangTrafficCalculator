fd818021a8eef75a2cc0af1cae7798c8b9a4f092
Matsvei Pahotski <matsvei.pahotski@gmail.com>
2022-01-20 11:15:09 +0100

    add functions to check entered values & add warning msgbox

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from src.ErlangModel import ErlangModelB
 from src.ClearWindow import clear_window
 
@@ -40,21 +41,53 @@ class Calculator:
         average_traffic_string_field = str(self.__average_traffic_text_field.get())
         number_of_lines_string_field = str(self.__number_of_lines_text_field.get())
 
+        average_traffic = None
+        number_of_lines = None
+
+        if self.__filed_data_is_ok(average_traffic_string_field):
+            average_traffic = self.__get_average_traffic_from(average_traffic_string_field)
+        else:
+            messagebox.showwarning('Warning', 'Bad value of Average traffic')
+
+        if self.__filed_data_is_ok(number_of_lines_string_field):
+            number_of_lines = self.__get_average_traffic_from(number_of_lines_string_field)
+        else:
+            messagebox.showwarning('Warning', 'Bad value of Number of lines')
+
+        if average_traffic and number_of_lines:
+            result = str(erlang_model_b.calculate_probability_of_blocking(average_traffic, number_of_lines))
+            result = result.translate({ord(i): None for i in '[]'})
+            self.__result_text_field.insert(0, result)
+
+    def __get_average_traffic_from(self, average_traffic_string_field):
         if self.__check_string_for_a_delimeter(average_traffic_string_field) is True:
-            min_value, max_value = average_traffic_string_field.split("-")
+            min_value, max_value = average_traffic_string_field.split(self.__special_character)
             average_traffic = [getint(min_value), getint(max_value)]
         else:
-            average_traffic = getint(self.__average_traffic_text_field.get())
+            average_traffic = getint(average_traffic_string_field)
+        return average_traffic
 
+    def __get_number_of_lines_from(self, number_of_lines_string_field):
         if self.__check_string_for_a_delimeter(number_of_lines_string_field) is True:
-            min_value, max_value = number_of_lines_string_field.split("-")
+            min_value, max_value = number_of_lines_string_field.split(self.__special_character)
             number_of_lines = [getint(min_value), getint(max_value)]
         else:
-            number_of_lines = getint(self.__number_of_lines_text_field.get())
+            number_of_lines = getint(number_of_lines_string_field)
+        return number_of_lines
 
-        result = str(erlang_model_b.calculate_probability_of_blocking(average_traffic, number_of_lines))
-        result = result.translate({ord(i): None for i in '[]'})
-        self.__result_text_field.insert(0, result)
+    def __filed_data_is_ok(self, string_field):
+        if self.__check_string_for_a_delimeter(string_field) is True:
+            min_value, max_value = string_field.split(self.__special_character)
+            if min_value.isnumeric() and max_value.isnumeric():
+                min_value, max_value = getint(min_value), getint(max_value)
+                if min_value > 0 and max_value > 0:
+                    return True
+        else:
+            if string_field.isnumeric():
+                number = getint(string_field)
+                if number > 0:
+                    return True
+        return False
 
     def __check_string_for_a_delimeter(self, string_field):
         if any(character in self.__special_character for character in string_field):
